688bda0adf9d32ce7e4563fc3d903c0214970855
Tibz <38702209+Tibzie@users.noreply.github.com>
2020-10-20 04:27:11 +0100

    new file

---
+++ inches_to_feet.js
@@ -0,0 +1,5 @@
+// Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet.
+
+function inchesToFeet(inches) {
+	return inches > 11 ? inches/12 : 0
+}
